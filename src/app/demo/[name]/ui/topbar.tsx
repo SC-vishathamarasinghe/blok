fcc452c282ff2bae49aee8a234306f84a167d7eb
Muthula <muhtula.dev@gmail.com>
2025-11-11 14:56:35 +0530

    Update the code format to align the code extracting logic

--- src/app/demo/[name]/ui/topbar.tsx
+++ src/app/demo/[name]/ui/topbar.tsx
@@ -1,13 +1,10 @@
 import Topbar from "@/components/ui/top-bar";
-import MinimalLayout from "../blocks/minimal-layout";
 
 export const topbar = {
   name: "topbar",
   components: {
     Default: (
-      <MinimalLayout>
-        <Topbar />
-      </MinimalLayout>
+      <Topbar />
     ),
   },
 };
