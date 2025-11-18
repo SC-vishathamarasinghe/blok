1d7e5efa8c868ccefa82e991e138e1b3d9d2d652
Muthula <muhtula.dev@gmail.com>
2025-11-18 16:44:16 +0530

    Update content in avatar demo

--- src/app/demo/[name]/ui/avatar.tsx
+++ src/app/demo/[name]/ui/avatar.tsx
@@ -12,7 +12,7 @@ export const avatar = {
   ),
   usage: [
     `import {\n  Avatar,\n  AvatarFallback,\n  AvatarImage,\n} from "@/components/ui/avatar"`,
-    `<Avatar>\n  <AvatarImage src="https://ca.slack-edge.com/T1S2RKGUA-U015TJJS8MS-b581732f917e-512" alt="Frank Grinaert" />\n  <AvatarFallback className="bg-primary text-primary-foreground">\n    <span className="text-xs">CN</span>\n  </AvatarFallback>\n</Avatar>`
+    `<Avatar>\n  <AvatarImage src="https://ca.slack-edge.com/T1S2RKGUA-U015TJJS8MS-b581732f917e-512" alt="Frank Grinaert" />\n  <AvatarFallback>\n    <span>CN</span>\n  </AvatarFallback>\n</Avatar>`
   ],
   components: {
     Fallback: (
