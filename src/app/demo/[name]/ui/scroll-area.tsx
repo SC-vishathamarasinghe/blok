07cdd8201e161df0cc7f6932ddabed4baae29a08
Muthula <muhtula.dev@gmail.com>
2025-11-18 14:03:19 +0530

    Update scroll area with updated details

--- src/app/demo/[name]/ui/scroll-area.tsx
+++ src/app/demo/[name]/ui/scroll-area.tsx
@@ -24,6 +24,25 @@ const tags = Array.from({ length: 50 }).map(
 
 export const scrollArea = {
   name: "scrollarea",
+  defaultComponent: (
+    <div className="flex flex-col gap-6">
+      <ScrollArea className="h-72 w-48 rounded-md border">
+        <div className="p-4">
+          <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
+          {tags.map((tag) => (
+            <React.Fragment key={tag}>
+              <div className="text-sm">{tag}</div>
+              <Separator className="my-2" />
+            </React.Fragment>
+          ))}
+        </div>
+      </ScrollArea>
+    </div>
+  ),
+  usage: [
+    `import { ScrollArea } from "@/components/ui/scroll-area";`,
+    `<ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">\n Jokester began sneaking into the castle in the middle of the night and leaving jokes all over the place: under the king's pillow, in his soup,\n even in the royal toilet. The king was furious, but he couldn't seem to stop Jokester. And then, one day, the people of the kingdom discovered\n that the jokes left by Jokester were so funny that they couldn't help but laugh. And once they started laughing, they couldn't stop.\n</ScrollArea>`,
+  ],
   components: {
     ScrollAreaVertical: (
       <div className="flex flex-col gap-6">
