e4ba446deb662ee2fbe02d87467d4373e2b5bed1
Muthula <muhtula.dev@gmail.com>
2025-11-18 11:38:37 +0530

    Update draggable with updated details

--- src/app/demo/[name]/ui/draggable.tsx
+++ src/app/demo/[name]/ui/draggable.tsx
@@ -4,6 +4,13 @@ import { CustomHandleDemo } from "./draggable-custom-handle";
 
 export const draggable = {
   name: "draggable",
+  defaultComponent: (
+    <BasicDragDrop />
+  ),
+  usage: [
+    `import {\n  DragDropProvider,\n  useDragDropContext,\n} from "@/components/ui/drag-drop-context";\nimport { Draggable } from "@/components/ui/draggable";\nimport { Droppable } from "@/components/ui/droppable";`,
+    `<DragDropProvider>\n  <DragDropDemo />\n</DragDropProvider>`,
+  ],
   components: {
     "Basic Drag and Drop": <BasicDragDrop />,
     "Sortable List": <SortableListDemo />,
