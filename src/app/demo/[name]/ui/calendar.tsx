255fef31cd506d31c5b61ce9e690cb13c00baed7
Muthula <muhtula.dev@gmail.com>
2025-11-18 11:11:48 +0530

    Update calendar with updated details

--- src/app/demo/[name]/ui/calendar.tsx
+++ src/app/demo/[name]/ui/calendar.tsx
@@ -7,10 +7,11 @@ export const calendar = {
     <SingleCalendar />
   ),
   usage: [
-    `import { SingleCalendar } from "@/app/demo/[name]/ui/single-calendar"`,
-    `<SingleCalendar />`
+    `import { Calendar } from "@/components/ui/calendar";`,
+    `const [date, setDate] = React.useState<Date | undefined>(new Date())\n\nreturn (\n  <Calendar\n    mode="single"\n    selected={date}\n    onSelect={setDate}\n    className="rounded-lg border"\n  />\n)`,
   ],
   components: {
+    "Single": <SingleCalendar />,
     "Two Months": <MultiCalendar numberOfMonths={2} />,
     "Three Months": <MultiCalendar numberOfMonths={3} />,
   },
