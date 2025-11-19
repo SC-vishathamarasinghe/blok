a529872e3b125cb1b6864121dbef58fbd06ebe46
Muthula <muhtula.dev@gmail.com>
2025-11-19 09:23:58 +0530

    Update content inradio group demo

--- src/app/demo/[name]/ui/radio-group.tsx
+++ src/app/demo/[name]/ui/radio-group.tsx
@@ -37,6 +37,6 @@ export const radioGroup = {
   ),
   usage: [
     `import {\n  RadioGroup,\n  RadioGroupItem,\n} from "@/components/ui/radio-group";`,
-    `<RadioGroup defaultValue="comfortable">\n <div className="flex items-center gap-3">\n  <RadioGroupItem value="default" id="r1" aria-label="Default option" />\n  <Label htmlFor="r1">Admin</Label>\n </div>\n</RadioGroup>`,
+    `<RadioGroup>\n <div className="flex items-center gap-3">\n  <RadioGroupItem value="default" id="r1" />\n  <Label htmlFor="r1">Option 1</Label>\n </div>\n</RadioGroup>`,
   ],
 };
