b1fd97bdee4ddbd058494870b84df05d9dbe0eeb
Muthula <muhtula.dev@gmail.com>
2025-11-19 10:08:33 +0530

    Update content in chart demo

--- src/app/demo/[name]/ui/chart.tsx
+++ src/app/demo/[name]/ui/chart.tsx
@@ -9,10 +9,6 @@ export const chart = {
   defaultComponent: (
     <AreaChartComponent />
   ),
-  usage: [
-    `import {\n  Area,\n  AreaChart,\n  CartesianGrid,\n  XAxis,\n} from "recharts"; \nimport {\n  ChartConfig,\n  ChartContainer,\n  ChartTooltip,\n  ChartTooltipContent,\n} from "@/components/ui/chart";`,
-    `<AreaChart>\n  <Area dataKey="desktop" type="natural" fill="var(--color-desktop)" fillOpacity={0.4} stroke="var(--color-desktop)" />\n</AreaChart>`,
-  ],
   components: {
     "Area Chart": <AreaChartComponent />,
     "Bar Chart": <BarChartComponent />,
