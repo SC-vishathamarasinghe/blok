6e89ca4a36ec21491544a8d89c4f50280408181d
Muthula <muhtula.dev@gmail.com>
2025-11-24 19:47:35 +0530

    Add json test

---
+++ src/components/demo-tab.tsx
@@ -0,0 +1,27 @@
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
+import { CodeBlock } from "@/components/code-block";
+import { ReactNode } from "react";
+
+interface DemoTabProps {
+    code: string;
+    component: ReactNode;
+    defaultTab?: "demo" | "code";
+}
+
+export default function DemoTab({ code, component, defaultTab = "demo" }: DemoTabProps) {
+    return (
+        <Tabs defaultValue={defaultTab}>
+            <TabsList>
+                <TabsTrigger value="demo">Demo</TabsTrigger>
+                <TabsTrigger value="code">Code</TabsTrigger>
+            </TabsList>
+            <TabsContent value="demo" className="min-h-[200px] p-8 bg-subtle-bg flex items-center justify-center rounded-md">
+                {component}
+            </TabsContent>
+
+            <TabsContent value="code">
+                <CodeBlock code={code} />
+            </TabsContent>
+        </Tabs>
+    );
+}
