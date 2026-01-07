5f17e1cd54c520412a04dd5b7a7fde71f508334b
sc-ishankalakshan <sc.ishanka@gmail.com>
2026-01-07 11:33:57 +0530

    dyanmic imports changes

--- src/app/demo/[name]/ui/stepper.tsx
+++ src/app/demo/[name]/ui/stepper.tsx
@@ -1,6 +1,4 @@
-import StepperExample from "./stepper-example";
-import StepperCurrentStep from "./stepper-current-step";
-import StepperVertical from "./stepper-vertical";
+import StepperExample from "@/app/demo/[name]/ui//stepper-example";
 
 export const stepper = {
   name: "stepper",
